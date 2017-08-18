350a0a1ec546ab6915747d90fd56b646dcfbd6be
lirenhao <anotherme@aliyun.com>
2017-08-18 17:17:38 +0800

    x

--- email_robot/send_email.py
+++ email_robot/send_email.py
@@ -1,3 +1,4 @@
+from email.header import Header
 from email.mime.text import MIMEText
 import smtplib
 
@@ -6,9 +7,11 @@ password = "a411867400"
 to_addr = "411867400@qq.com"
 smtp_server = "smtp.aliyun.com"
 msg = MIMEText('测试邮件内容', 'plain', 'utf-8')
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = Header(u'测试邮件', 'utf-8').encode()
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
-
